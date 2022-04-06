93157aca4b432a7d75b2aa755a877de60d37e317
Raizer Varela <raizer.varela@gmail.com>
2022-04-06 11:24:46 -0300

    Adição - Field, componente mina e seus estados

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {StyleSheet, Text, View} from 'react-native'
 import params from './src/params'
-import Field from './src/componemts/Field'
+import Field from './src/components/Field'
 
 export default class App extends Component{
   render(){
@@ -12,9 +12,17 @@ export default class App extends Component{
           {params.getRowsAmount()}x{params.getCollumnsAmount()}
         </Text>
         <Field/>
+        <Field opened/>
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={3}/>
+        <Field opened nearMines={6}/>
+        <Field mined/>
+        <Field mined opened/>
+        <Field mined opened exploded/>
       </View>
     )
-  }
+  } 
 }
 
 const styles = StyleSheet.create({
